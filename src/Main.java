01993b5645d79df094776df236ee57726697a395
Stretch Projects <StretchProjects@gmail.com>
2014-10-10 20:18:33 +0100

    Files to accompany Youtube series

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+/**
+ * Author   : Stretch Projects
+ * Series   : Introduction to Java
+ * Topic    : Variables
+ * Lecture  : http://youtu.be/z_fNvob19IQ
+ * Tutorial : http://youtu.be/XPu3tHPQsXw
+ * License  : None.  Do with it as you wish! :)
+ */
+
+public class Main {
+
+	public static void main(String[] args) {	
+		int viewsByJohn = 6;
+		
+		int viewsByAmy = 7;
+		
+		int totalViews = viewsByJohn + viewsByAmy;
+		
+		String message = "Total views: ";
+		
+		System.out.println(message+totalViews);
+	}
+
+}
